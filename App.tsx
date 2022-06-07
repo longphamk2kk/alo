e8e933f50329c2c7324abcab49c5028d8db1f808
longphamk2kk <71912566+longphamk2kk@users.noreply.github.com>
2022-06-07 08:29:53 +0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './src/designSystem/TextStyle';
+import './src/designSystem/colors';
+import './src/designSystem/assets';
+import RootStack from './src/nav/RootStack';
+
+const App = () => {
+  return <RootStack />;
+};
+
+export default App;
